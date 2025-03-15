84bacdfa20fd7083e6d84210b8652e18beb77493
emereson <escanorrosa123@gmail.com>
2025-03-15 00:25:43 -0500

    vvv

--- src/app/(pages)/intranet/(pages)/login/page.tsx
+++ src/app/(pages)/intranet/(pages)/login/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useState } from 'react'
 import './login.css'
-import { useRouter } from 'next/navigation'
 import axios from 'axios'
 import Loading from '@/hooks/Loading'
 import Input from '@/hooks/input/Input'
